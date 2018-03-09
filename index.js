96b41644989184c26df7e6808506c103883e3708
Joseph Higgins <jopheshiggins@gmail.com>
2018-03-09 00:59:43 +0000

    Added picture stuff

--- index.js
+++ index.js
@@ -51,21 +51,64 @@ creatures.forEach(function(creature)
     var input = [];
     for (var i in creatures)
     {
-      input.push(creatures[i].location.x);
-      input.push(creatures[i].location.y);
-      input.push(creatures[i].velocity.x);
-      input.push(creatures[i].velocity.y);
+        input.push(creatures[i].location.x);
+        input.push(creatures[i].location.y);
+        input.push(creatures[i].velocity.x);
+        input.push(creatures[i].velocity.y);
     }
     var output = creature.network.activate(input);
     creature.moveTo(output);
-    
+
     // learn
     var learningRate = .3;
     var target = [
-      targetX(creature), 
-      targetY(creature), 
-      targetAngle(creature)];
+        targetX(creature), 
+        targetY(creature), 
+        targetAngle(creature)];
     creature.network.propagate(learningRate, target);
 });*/
 
-var network = new Architect.Perceptron(2, 512, 1);
+var PNG = require('pngjs').PNG;
+var fs = require('fs');
+var perceptron = new Architect.Perceptron(2, 96, 3);
+fs.createReadStream('Images/Pug1/pug-100.png').pipe(new PNG({filterType: 4})).on('parsed', function() {
+    var pixels = [];
+    for (const i in this.data) {
+        if (this.data.hasOwnProperty(i)) {
+            pixels.push(this.data[i] / 255);
+        }
+    }
+
+    for (let g = 0; g <= 512*16*2; g++) {
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+				var dynamicRate = .01/(1+.0005*g);
+                var id = (this.width * y + x) * 4;
+                perceptron.activate([x / this.width, y / this.height]);
+                perceptron.propagate(dynamicRate, [pixels[id], pixels[id+1], pixels[id+2]]);
+            }
+        }
+        if (g % 8 == 0) {
+            var genPixels = [];
+            for (let x = 0; x < this.width; x++) {
+                for (let y = 0; y < this.height; y++) {
+                    var id = (this.width * y + x) * 4;
+                    var gen = perceptron.activate([x / this.width, y / this.height]);
+                    for (let i = 0; i < gen.length; i++) {
+                        genPixels[id + i] = gen[i] * 255;
+                    }
+                    genPixels[id + 3] = pixels[id + 3] * 255;
+                }
+            }
+            var newPug = new PNG({ filterType:4, width: this.width, height: this.height });
+            for (let i = 0; i < genPixels.length; i++) {
+                newPug.data[i] = genPixels[i];
+            }
+            //newPug.pack().pipe(fs.createWriteStream('Out/out-' + g + '.png'));
+            //newPug.pack().pipe(fs.createWriteStream('Out/out-' + g + '.png'));
+            var buffer = PNG.sync.write(newPug, { colorType: 6 });
+            fs.writeFileSync('Out/out-'+g+'.png', buffer);
+            console.log(g);
+        }
+    }
+});
